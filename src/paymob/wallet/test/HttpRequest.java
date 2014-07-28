63e209ff6c169f85acec8b9c8a33cdf87f709549
menessy <menessy@paymobsolutions.com>
2014-07-29 01:54:06 +0300

    initial

---
+++ src/paymob/wallet/test/HttpRequest.java
@@ -0,0 +1,145 @@
+package paymob.wallet.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+import javax.net.ssl.SSLSocketFactory;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import org.json.simple.JSONObject;
+
+public class HttpRequest {
+
+	private static String backend_url = "http://127.0.0.1:8000/mobile_api/";
+	private static HashMap<Function, String> methods = new HashMap<Function, String>();
+	private static boolean isReady = false;
+
+	public static enum Function {
+		ACTIVATE_WALLET, LOGIN, SEND_MSG, SYNC_MSG, SYNC_CONTACTS,
+	}
+
+	private static void init() {
+		methods.put(Function.LOGIN, "login");
+		methods.put(Function.SEND_MSG, "send_message");
+		methods.put(Function.ACTIVATE_WALLET, "activate_wallet");
+		methods.put(Function.SYNC_MSG, "sync/message");
+		methods.put(Function.SYNC_CONTACTS, "sync/contacts");
+	}
+
+	public static String executeHttpRequest(JSONObject jsonObj, Function func) {
+
+		if (!isReady) {
+			init();
+			isReady = true;
+		}
+
+		String rawData = jsonObj.toString();
+		String type = "application/x-www-form-urlencoded";
+		String encodedData = URLEncoder.encode(rawData);
+		URL u;
+		HttpURLConnection conn;
+		InputStream is;
+		ByteArrayOutputStream os2 = new ByteArrayOutputStream();
+
+		byte[] buf = new byte[4096];
+		try {
+			u = new URL(backend_url + methods.get(func));
+			conn = (HttpURLConnection) u.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", type);
+			conn.setRequestProperty("Content-Length",
+					String.valueOf(encodedData.length()));
+			OutputStream os = conn.getOutputStream();
+			os.write(encodedData.getBytes());
+
+			if (conn.getResponseCode() == 200) {
+				is = conn.getInputStream();
+			} else {
+			    is = conn.getErrorStream();
+			    PrintWriter writer;
+			    int ret = 0;
+				while ((ret = is.read(buf)) > 0) {
+					os2.write(buf, 0, ret);
+				}
+				try {
+					
+					writer = new PrintWriter("/root/wallet_debug.html", "UTF-8");
+					writer.println(new String(os2.toByteArray()));
+					writer.close();
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			int ret = 0;
+			while ((ret = is.read(buf)) > 0) {
+				os2.write(buf, 0, ret);
+			}
+			// close the inputstream
+			is.close();
+			return new String(os2.toByteArray());
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			 
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			
+		}
+
+		return encodedData;
+
+	}
+
+	public static HttpClient getNewHttpClient() {
+
+		try {
+
+			HttpParams params = new BasicHttpParams();
+			HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+			HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+			SchemeRegistry registry = new SchemeRegistry();
+			registry.register(new Scheme("http", PlainSocketFactory
+					.getSocketFactory(), 8000));
+			// registry.register(new Scheme("https", sf, 443));
+
+			ClientConnectionManager ccm = new ThreadSafeClientConnManager(
+					params, registry);
+
+			return new DefaultHttpClient(ccm, params);
+
+		} catch (Exception e) {
+			return new DefaultHttpClient();
+		}
+
+	}
+}
---
+++ src/paymob/wallet/test/Mobile.java
@@ -0,0 +1,162 @@
+package paymob.wallet.test;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.bouncycastle.util.encoders.Hex;
+import org.bouncycastle.util.io.pem.PemWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
+import javax.xml.bind.DatatypeConverter;
+
+public class Mobile {
+
+	private String device_id = "my device id";
+	private String sim_serial_id = "my serial sim id";
+	public String app_id;
+	private String mobile_number = "01005558658";
+
+	private String gcm_id = "gcm_id";
+	private String adm_id = "adm_id";
+
+	private PublicKey publicKey;
+	private String private_key_not_to_be_stored_on_mobile;
+
+	
+	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+	public static String bytesToHex(byte[] bytes) {
+	    char[] hexChars = new char[bytes.length * 2];
+	    for ( int j = 0; j < bytes.length; j++ ) {
+	        int v = bytes[j] & 0xFF;
+	        hexChars[j * 2] = hexArray[v >>> 4];
+	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	    }
+	    return new String(hexChars);
+	}
+	
+	
+	public void generate_certificate() {
+		KeyPairGenerator keyPairGenerator;
+		try {
+			keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+			keyPairGenerator.initialize(2048, new SecureRandom());
+			KeyPair keyPair = keyPairGenerator.genKeyPair();
+			publicKey = keyPair.getPublic();
+
+			private_key_not_to_be_stored_on_mobile = DatatypeConverter
+					.printBase64Binary(keyPair.getPrivate().getEncoded());
+
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void activate(String activation_code) {
+		if (publicKey == null)
+			generate_certificate();
+
+		JSONObject jsonobj = new JSONObject();
+		JSONArray list = new JSONArray();
+		list.add(activation_code);
+		list.add(gcm_id);
+		list.add(adm_id);
+		list.add(mobile_number);
+		list.add(device_id);
+		list.add(sim_serial_id);
+		list.add(private_key_not_to_be_stored_on_mobile);
+
+		jsonobj.put("account", list);
+		String request = HttpRequest.executeHttpRequest(jsonobj,
+				HttpRequest.Function.ACTIVATE_WALLET);
+
+		JSONParser parser = new JSONParser();
+		try {
+			JSONObject jsonObject = (JSONObject) parser.parse(request);
+			JSONArray lang = (JSONArray) jsonObject.get("details");
+			JSONObject innerObj = (JSONObject) lang.get(0);
+			app_id = (String) innerObj.get("app_id");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println(request);
+
+	}
+
+	public static String encrypt(byte[] plain, PublicKey key) {
+		byte[] cipherText = null;
+		try {
+			// get an RSA cipher object and print the provider
+			final Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			// encrypt the plain text using the public key
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			System.out.println( new String(plain, "UTF-8"));			
+			cipherText = cipher.doFinal(plain);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return bytesToHex(cipherText);
+	}
+
+	public String generate_hash(String key_per_session_120_bit) {
+		// imp note : the mobile number should be in the following form 2010xxx
+		// ( pre-pended by 2 )
+		String data = this.mobile_number + this.device_id + this.sim_serial_id
+				+ this.app_id + key_per_session_120_bit;
+		MessageDigest mda;
+		try {
+			mda = MessageDigest.getInstance("SHA-512");
+			byte[] digesta = mda.digest(data.getBytes());
+			
+				return bytesToHex(digesta);
+				
+			
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		return null;
+	}
+
+	public void login() {
+		SecureRandom random = new SecureRandom();
+		String r = bytesToHex(random.generateSeed(120));
+		String encrypted_value = generate_hash(r);
+		
+		String encrypted_hash = encrypt(encrypted_value.getBytes(), publicKey);
+		String key_per_session = encrypt(r.getBytes(), publicKey);
+
+		
+		
+		JSONObject jsonobj = new JSONObject();
+		JSONArray list = new JSONArray();
+		list.add(encrypted_hash);
+		list.add(app_id);
+		list.add(key_per_session);
+
+		jsonobj.put("account", list);
+
+		String request = HttpRequest.executeHttpRequest(jsonobj,
+				HttpRequest.Function.LOGIN);
+		System.out.println(request);
+	}
+}
