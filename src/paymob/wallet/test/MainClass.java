eb3d08e4fadca6a83db003c657243198981213e5
menessy <menessy@paymobsolutions.com>
2014-08-12 17:08:10 +0300

    done

--- src/paymob/wallet/test/MainClass.java
+++ src/paymob/wallet/test/MainClass.java
@@ -2,9 +2,13 @@ package paymob.wallet.test;
 
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
@@ -14,18 +18,34 @@ public class MainClass {
 
         String content;
 
-	public static void main(String[] args) throws IOException  {
+	public static void main(String[] args) throws IOException, IllegalBlockSizeException, UnsupportedEncodingException, BadPaddingException  {
                 System.out.println("Hello first");
                 
                 Mobile mob = new Mobile();
                 System.out.println("Activation Request is occuring");
-                mob.activate("jcuyznt");
+                mob.activate("7ijf44v");
                 
                 System.out.println("Generating Login Request");
                 mob.login();
                 
                 System.out.println("Sending Message");
-                mob.sendMessage("a msg");
+                JSONArray mobiles = new JSONArray();
+                mobiles.add("201005087044");
+                mobiles.add("201005087047");
+                mobiles.add("201005087048");
+                mob.sendNormalMessage("a msg","",mobiles);
+                
+                
+                Date startDate = new Date();
+                Date  endDate = new Date();
+                Frequency obj = new Frequency(startDate,endDate,"3","12","",
+                        Frequency.StartFrequencyType.MONTHLY,
+                        Frequency.EndFrequencyType.FOREVER);
+                mob.sendTransactionRequest("transaction req", 40.0, obj,"",mobiles);
+                
+                
+                System.out.println("Sending Transaction Reply");
+                mob.sendTransactionReply("reply", "22", "2");
                 //System.out.println("Generating normal send message request");
                 //x.genNormalReq();
                 
