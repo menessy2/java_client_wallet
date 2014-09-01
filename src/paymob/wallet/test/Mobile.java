2d570c3c9a43fba5858b6db673fba09be6a9a8fe
menessy <menessy@paymobsolutions.com>
2014-09-01 12:22:42 +0300

    fully tested

--- src/paymob/wallet/test/Mobile.java
+++ src/paymob/wallet/test/Mobile.java
@@ -42,15 +42,15 @@ import sun.misc.BASE64Decoder;
 
 public class Mobile {
 
-    private String device_id = "my_device_id";      // spaces are not allowed in such variable
-    private String sim_serial_id = "my_serial_sim_id"; // spaces are not allowed in such variable
+    private String device_id;      // spaces are not allowed in such variable
+    private String sim_serial_id; // spaces are not allowed in such variable
     public String app_id;   // variable given by the server during activation and is permanently
     // stored on the mobile application since then. 
-    private String mobile_number = "201005558658";
+    private String mobile_number;
     // any mobile number stored on the mobile should be appended by 2 , for example : 2010..
 
-    private String gcm_id = "gcm_id";
-    private String adm_id = "adm_id";
+    private String gcm_id;
+    private String adm_id;
 
     private PublicKey publicKey;
     private String private_key_not_to_be_stored_on_mobile;
@@ -59,50 +59,32 @@ public class Mobile {
     private String key_per_session;
 
     final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-
     private static final Charset ASCII = Charset.forName("US-ASCII");
+    public static String initializationV = "ZgP#d_qH543LgpS-";
     
-    public static String bytesToHex(byte[] bytes) {
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
-            int v = bytes[j] & 0xFF;
-            hexChars[j * 2] = hexArray[v >>> 4];
-            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
-        }
-        return new String(hexChars);
-    }
 
-    public String generateRandomString() {
-        char[] chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=!@#$%^&*()_+~".toCharArray();
-        StringBuilder sb = new StringBuilder();
-        SecureRandom random = new SecureRandom();
-        for (int i = 0; i < 32; i++) {
-            char c = chars[random.nextInt(chars.length)];
-            sb.append(c);
-        }
-        String output = sb.toString();
-        return output;
+    public Mobile(String mobileNumber,String deviceID,String simSerialID,
+            String gcmID,String admID) {
+        this.mobile_number = mobileNumber;
+        this.device_id = deviceID;
+        this.sim_serial_id = simSerialID;
+        this.gcm_id = gcmID;
+        this.adm_id = admID;
     }
-
-    public void generate_certificate() {
-        KeyPairGenerator keyPairGenerator;
-        try {
-            keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-
-            keyPairGenerator.initialize(2048, new SecureRandom());
-            KeyPair keyPair = keyPairGenerator.genKeyPair();
-            publicKey = keyPair.getPublic();
-            private_key_not_to_be_stored_on_mobile = DatatypeConverter
-                    .printBase64Binary(keyPair.getPrivate().getEncoded());
-        } catch (NoSuchAlgorithmException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+    
+    
+    public String getHashVariables(String keypersession){
+        return this.mobile_number + this.device_id + this.sim_serial_id
+                + this.app_id + keypersession;
     }
+    
 
     public void activate(String activation_code) {
         if (publicKey == null) {
-            generate_certificate();
+            KeyPair keyPair = Security.generate_certificate();
+            publicKey = keyPair.getPublic();
+            private_key_not_to_be_stored_on_mobile = DatatypeConverter
+                    .printBase64Binary(keyPair.getPrivate().getEncoded());
         }
 
         JSONObject jsonobj = new JSONObject();
@@ -132,48 +114,17 @@ public class Mobile {
 
     }
 
-    public static String encrypt(byte[] plain, PublicKey key) {
-        byte[] cipherText = null;
-        try {
-            // get an RSA cipher object and print the provider
-            final Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
-            // encrypt the plain text using the public key
-            cipher.init(Cipher.ENCRYPT_MODE, key);
-            cipherText = cipher.doFinal(plain);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return DatatypeConverter.printBase64Binary(cipherText);
-    }
-
-    public byte[] generate_hash(String key_per_session_256_bit) {
-		// imp note : the mobile number should be in the following form 2010xxx
-        // ( pre-pended by 2 )
-        String data = this.mobile_number + this.device_id + this.sim_serial_id
-                + this.app_id + key_per_session_256_bit;
-        MessageDigest mda;
-        try {
-            mda = MessageDigest.getInstance("SHA-512");
-            byte[] digesta = mda.digest(data.getBytes());
-
-            return digesta;
-
-        } catch (NoSuchAlgorithmException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return null;
-    }
+    
 
     public void login() {
 
         try {
 
-            String key_per_session = generateRandomString();
-            byte[] encrypted_value = generate_hash(key_per_session);
+            String key_per_session = Utils.generateRandomString();
+            byte[] encrypted_value = Security.generate_hash(this,key_per_session);
 
-            String encrypted_hash = encrypt(encrypted_value, publicKey);
-            String _key_per_session = encrypt(key_per_session.getBytes(), publicKey);
+            String encrypted_hash = Security.encrypt(encrypted_value, publicKey);
+            String _key_per_session = Security.encrypt(key_per_session.getBytes(), publicKey);
             JSONObject jsonobj = new JSONObject();
             JSONArray list = new JSONArray();
             list.add(encrypted_hash);
@@ -205,7 +156,7 @@ public class Mobile {
             BASE64Decoder base64decoder = new BASE64Decoder();
             byte[] cleartext = base64decoder.decodeBuffer(request);
 
-            byte[] req = decrypt_aes(cleartext, key_per_session, "ZgP#d_qH543LgpS-");
+            byte[] req = Security.decrypt_aes(cleartext, key_per_session, initializationV);
 
             JSONParser jsonParser = new JSONParser();
             JSONObject jsonObject = (JSONObject) jsonParser.parse(new String(req));
@@ -223,20 +174,7 @@ public class Mobile {
     }
 
     
-    public JSONObject parseResponse(String response) throws IOException, ParseException{
-       
-            BASE64Decoder base64decoder = new BASE64Decoder();
-            byte[] cleartext = base64decoder.decodeBuffer(response);
-
-            byte[] req = decrypt_aes(cleartext, key_per_session, "ZgP#d_qH543LgpS-");
-
-            JSONParser jsonParser = new JSONParser();
-            JSONObject jsonObject = (JSONObject) jsonParser.parse(new String(req));
-            JSONArray myjson = (JSONArray) jsonObject.get("details");
-            JSONObject myobject = (JSONObject) myjson.get(0);
-            
-            return myobject;
-    }
+    
     
     
     public void sendNormalMessage(String content, String threadID,JSONArray mobiles) throws UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
@@ -260,7 +198,7 @@ public class Mobile {
                 HttpRequest.Function.SEND_MSG, this.key_per_session, this.session);
         
         try {
-            JSONObject obj = parseResponse(request);
+            JSONObject obj = HttpRequest.parseResponse(request,this.key_per_session);
             System.out.println(obj.toJSONString());
             String thread_id = (String) obj.get("thread_id");
             String message_id = (String) obj.get("message_id");
@@ -304,7 +242,7 @@ public class Mobile {
                 HttpRequest.Function.SEND_MSG, this.key_per_session, this.session);
         
         try {
-            JSONObject obj = parseResponse(request);
+            JSONObject obj = HttpRequest.parseResponse(request,key_per_session);
             System.out.println(obj.toJSONString());
             String thread_id = (String) obj.get("thread_id");
             String message_id = (String) obj.get("message_id");
@@ -338,7 +276,7 @@ public class Mobile {
                 HttpRequest.Function.SEND_MSG, this.key_per_session, this.session);
         
         try {
-            JSONObject obj = parseResponse(request);
+            JSONObject obj = HttpRequest.parseResponse(request,this.key_per_session);
             System.out.println(obj.toJSONString());
             String thread_id = (String) obj.get("thread_id");
             String message_id = (String) obj.get("message_id");
@@ -375,7 +313,7 @@ public class Mobile {
                 HttpRequest.Function.SEND_MSG, this.key_per_session, this.session);
         
         try {
-            JSONObject obj = parseResponse(request);
+            JSONObject obj = HttpRequest.parseResponse(request,this.key_per_session);
             System.out.println(obj.toJSONString());
             String thread_id = (String) obj.get("thread_id");
             String message_id = (String) obj.get("message_id");
@@ -399,7 +337,7 @@ public class Mobile {
                 HttpRequest.Function.SYNC_CONTACTS, this.key_per_session, this.session);
         
         try {
-            JSONObject obj = parseResponse(request);
+            JSONObject obj = HttpRequest.parseResponse(request,this.key_per_session);
             System.out.println(obj.toJSONString());
             
         } catch (IOException ex) {
@@ -416,7 +354,7 @@ public class Mobile {
                 HttpRequest.Function.SYNC_MSG, this.key_per_session, this.session);
         
         try {
-            JSONObject obj = parseResponse(request);
+            JSONObject obj = HttpRequest.parseResponse(request,this.key_per_session);
             System.out.println(obj.toJSONString());
             
         } catch (IOException ex) {
@@ -425,64 +363,6 @@ public class Mobile {
             Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-    }
-    
-    public static byte[] decrypt_aes(byte[] plainText, String encryptionKey, String IV) {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-            SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes(), "AES");
-            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF8")));
-
-            return cipher.doFinal(plainText);
-        } catch (InvalidKeyException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InvalidAlgorithmParameterException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IllegalBlockSizeException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (BadPaddingException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchPaddingException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return null;
-    }
-    
-    
-    public static byte[] encrypt_aes(byte[] plainText, String encryptionKey, String IV) throws IllegalBlockSizeException, BadPaddingException {
-        try {
-            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
-            SecretKeySpec key = new SecretKeySpec(encryptionKey.getBytes(), "AES");
-            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(IV.getBytes("UTF8")));
-            int padding = (16 - (plainText.length % 16));
-            byte[] padded_value = new byte[padding];
-            for (int i = 0; i < padding; i++) {
-                char a = ' ';
-                padded_value[i] = (byte) a;
-            }
-            byte[] combined = new byte[padded_value.length + plainText.length];
-
-            System.arraycopy(plainText, 0, combined, 0, plainText.length);
-            System.arraycopy(padded_value, 0, combined, plainText.length, padded_value.length);
-            return cipher.doFinal(combined);
-        } catch (InvalidKeyException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (InvalidAlgorithmParameterException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchPaddingException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Mobile.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        return null;
     }
 
 }
